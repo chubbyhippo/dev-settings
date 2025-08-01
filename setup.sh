41fb651e1f4c8dada54df06f3d4c2a76b678d732
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2025-08-01 21:34:07 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,3 +4,6 @@
 curl https://raw.githubusercontent.com/chubbyhippo/.vimrc/refs/heads/master/.vimrc -o ~/.vimrc
 
 # neovim
+[ -d "${XDG_CONFIG_HOME:-$HOME/.config}/nvim" ] && rm -rf "${XDG_CONFIG_HOME:-$HOME/.config}/nvim"; mkdir -p "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim && curl https://raw.githubusercontent.com/chubbyhippo/init.lua/main/init.lua -o "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim/init.lua
+
+# intellij
